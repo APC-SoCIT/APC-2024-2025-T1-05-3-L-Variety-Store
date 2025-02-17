3a87dac32de598c4d61ab23f2c3917e56490aaae
Rainier Edward Lopez <155550561+rainieredward@users.noreply.github.com>
2025-02-17 16:32:02 +0800

    Update urls.py

--- myproject/pos_front/urls.py
+++ myproject/pos_front/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import scan_barcode, storefront_view
+from . import views
 
 urlpatterns = [
-    path('scan/', scan_barcode, name='scan_barcode'),
-    path('Storefront/', storefront_view, name='Sale'),
-
+    path('', views.home, name='home'),
+    path('create_order/', views.create_order, name='create_order'),
+    path('process_payment/<int:order_id>/', views.process_payment, name='process_payment'),
 ]
