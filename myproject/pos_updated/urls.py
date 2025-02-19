44ea85a68283a481144b32d3b3140eccf514a9b4
Rainier Edward Lopez <155550561+rainieredward@users.noreply.github.com>
2025-02-20 02:02:59 +0800

    Delete myproject/pos_updated/urls.py

--- myproject/pos_updated/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('pos/', views.pos_view, name='pos'),
-    path('receipt/<int:sale_id>/', views.receipt_view, name='receipt'),
-]
