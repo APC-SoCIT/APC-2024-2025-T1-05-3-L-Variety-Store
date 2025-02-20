511481d4063c5f73447398cfe129b1dcda1ae1b6
Timothy Louise R. Perez <71299474+apctimperez@users.noreply.github.com>
2025-02-20 08:26:39 +0800

    added templates settings.py

--- myproject/pos_updated/pos_project/settings.py
+++ myproject/pos_updated/pos_project/settings.py
@@ -21,6 +21,14 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-# Okay na ata to tim
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [BASE_DIR / 'templates'],
+        'APP_DIRS': True,
+    },
+]
