79a53fa698a12da0f12767b95017c003d0eab444
Timothy Louise R. Perez <71299474+apctimperez@users.noreply.github.com>
2025-02-20 16:55:54 +0800

    Added models.py

--- myproject/my_project/my-django-pos/pos/models.py
+++ myproject/my_project/my-django-pos/pos/models.py
@@ -16,7 +16,7 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
-
+# Models
 class Transaction(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField()
